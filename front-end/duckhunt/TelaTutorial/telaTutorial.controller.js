e060586fe7887361b2134d5c6edaea3526a7c2e3
Unknown <pablo.garcia@cwi.com.br>
2017-12-10 04:02:16 -0200

    feat(tcc) adicionando morte do pato, na tela tutorial

--- front-end/duckhunt/TelaTutorial/telaTutorial.controller.js
+++ front-end/duckhunt/TelaTutorial/telaTutorial.controller.js
@@ -3,21 +3,35 @@ angular.module('duckHunt').controller('tutorialController', function ($scope, du
     $scope.miraX = -300;
     $scope.miraY = -300;
 
-    $scope.patoX = -30;
-    $scope.patoY = -30;
+    $scope.patoX = 300;
+    $scope.patoY = 300;
 
     //Recebe mensagens do servidor
     $scope.$on('messageAdded', function (event, gamma, alpha, tiros) {
         $scope.tiros = tiros;
         $scope.miraX = gamma;
         $scope.miraY = alpha;
-        $scope.pato = "top:" + ($scope.patoY) + "%;";
+        $scope.pato = "left:" + ($scope.patoX) + "px; top:" + ($scope.patoY) +"px;";
         $scope.mira = "left:" + ($scope.miraX) + "px; top:" + ($scope.miraY) + "px;";
         $scope.$apply();
     });
 
     $scope.$on('atirou', function (event, acertou) {
         console.log(acertou);
+        if(acertou){
+            var patoMorto = document.getElementById("pato");
+            
+            patoMorto.style.animationPlayState = "paused";
+            patoMorto.style.animationName = "pato-morrendo";
+            patoMorto.style.animationFillMode = "forwards";
+            patoMorto.style.animationIterationCount = "1";
+            patoMorto.style.animationTimingFunction = "steps(1)";
+            patoMorto.style.animationDuration = "1s";
+            patoMorto.style.animationPlayState = "running";
+            //patoMorto.style.backgroundPosition = "-1655px -3px";
+            
+            //patoMorto.style.animationPlayState = "running";
+        }
     });
 
 
