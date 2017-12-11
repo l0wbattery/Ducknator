1b3bda732aef4feafade192f83f1a5c71c7150ca
bel.cogo <bel.cogo27@gmail.com>
2017-12-11 17:40:55 -0200

    feat(DUCKNATOR): implementando classe sala.

---
+++ back-end/SignalRSelfHost/SignalRSelfHost.Dominio/Entidades/Sala.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRSelfHost.Dominio.Entidades
+{
+    public class Sala
+    {
+        public int Id { get; private set; }
+        public int Token { get; private set; }
+        public String NomeUsuario { get; private set; }
+        public DateTime Data { get; private set; }
+        public int Pontos { get; private set; }
+        public int Nivel { get; private set; }
+        public int NumeroUsuarios { get; private set; }
+        public bool isAtiva { get; private set; }
+
+        public Sala()
+        {
+
+        }
+
+        public Sala(int token)
+        {
+            this.Nivel = 1;
+            this.Pontos = 0;
+            this.Data = DateTime.Now;
+            this.NomeUsuario = "Nickname";
+            this.Token = Token;
+            this.NumeroUsuarios = 1;
+            this.isAtiva = true;
+        }
+
+        public void Update(String nome)
+        {
+            this.NomeUsuario = nome;
+            this.NumeroUsuarios += 1;
+        }
+
+        public void LevelUp()
+        {
+            this.Nivel += 1;
+        }
+
+        public void Matei(int pontos)
+        {
+            this.Pontos += pontos;
+        }
+
+        public void ConexaoEncerrada()
+        {
+            this.isAtiva = false;
+        }
+    }
+}
