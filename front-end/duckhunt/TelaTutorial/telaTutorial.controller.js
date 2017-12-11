1e1bd5a36fff04598a0c9f65ed2e3771a149a756
Unknown <pablo.garcia@cwi.com.br>
2017-12-11 02:19:42 -0200

    feat(tcc) finalizando tela de tutorial

--- front-end/duckhunt/TelaTutorial/telaTutorial.controller.js
+++ front-end/duckhunt/TelaTutorial/telaTutorial.controller.js
@@ -1,37 +1,72 @@
 angular.module('duckHunt').controller('tutorialController', function ($scope, duckService) {
 
-    $scope.miraX = -300;
+    $scope.miraX = -400;
     $scope.miraY = -300;
 
-    $scope.patoX = 300;
-    $scope.patoY = 300;
+    var patoUmVivo = true;
+    var patoMorto = document.getElementById("pato");
+
+    $scope.rodaRound = function(){
+        duckService.rodaPatosMiniRound();
+    }
 
     //Recebe mensagens do servidor
     $scope.$on('messageAdded', function (event, gamma, alpha, tiros) {
         $scope.tiros = tiros;
         $scope.miraX = gamma;
         $scope.miraY = alpha;
-        $scope.pato = "left:" + ($scope.patoX) + "px; top:" + ($scope.patoY) +"px;";
+
         $scope.mira = "left:" + ($scope.miraX) + "px; top:" + ($scope.miraY) + "px;";
         $scope.$apply();
     });
 
-    $scope.$on('atirou', function (event, acertou) {
-        console.log(acertou);
-        if(acertou){
-            var patoMorto = document.getElementById("pato");
+    //Verifica se a mira está dentro do hitbox do pato;
+    function verificaSeAcertouPatoTutorial() {
+        if (($scope.miraY > (patoMorto.offsetTop) - 33) &&
+            ($scope.miraY < (patoMorto.offsetTop) + 33) &&
+            ($scope.miraX > (patoMorto.offsetLeft) - 41) &&
+            ($scope.miraX < (patoMorto.offsetLeft) + 41)) {
             
-            patoMorto.style.animationPlayState = "paused";
-            patoMorto.style.animationName = "pato-morrendo";
-            patoMorto.style.animationFillMode = "forwards";
-            patoMorto.style.animationIterationCount = "1";
-            patoMorto.style.animationTimingFunction = "steps(1)";
-            patoMorto.style.animationDuration = "1s";
-            patoMorto.style.animationPlayState = "running";
+            eliminarPato();
         }
+    }
+
+    //Realiza a nimação do pato sendo atingido e chama a função para faze-lo "cair";
+    function eliminarPato() {
+        patoMorto.style.animationName = "pato-morrendo";
+        patoMorto.style.animationFillMode = "forwards";
+        patoMorto.style.animationIterationCount = "1";
+        patoMorto.style.animationTimingFunction = "steps(1)";
+        patoMorto.style.animationDuration = "0.5s";
+        patoMorto.addEventListener("animationend", tirarPatoDaTela);
+    }
+
+    //Faz o pato "cair" após ser atingido e inicia função de remoção;
+    function tirarPatoDaTela() {
+        patoMorto.style.animationName = "pato-sumindo";
+        patoMorto.style.animationFillMode = "forwards";
+        patoMorto.style.animationIterationCount = "1";
+        patoMorto.style.animationTimingFunction = "linear";
+        patoMorto.style.animationDuration = "1s";
+        patoMorto.addEventListener("animationend", removerDiv);
+    }
+
+    //Remove DIV do pato tutorial e inicia o jogo ao fim da animação;
+    function removerDiv() {
+        patoMorto.style.display = "none";
+        patoMorto.addEventListener("animationend", iniciarJogo)
+    }
+
+    //Emvia para o backend o comando para iniciar o jogo;
+    function iniciarJogo() {
+        console.log("iniciando");
+    }
+
+    //Inicia verificação de acerto do pato tutorial ao receber o sinal de tiro vindo do backend;
+    $scope.$on('atirou', function (event, acertou) {
+        //console.log(acertou);
+        verificaSeAcertouPatoTutorial();
     });
-
-
 });
 
 
